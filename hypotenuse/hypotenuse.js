51aa4e7428853192b04b779fbd280bc632632f1e
your_username <kironpaul889@gmail.com>
2021-08-08 00:53:15 +0530

    Docs - Hypotenous UI | Function Done

--- hypotenuse/hypotenuse.js
+++ hypotenuse/hypotenuse.js
@@ -1,5 +1,10 @@
+let base = document.querySelector("#base")
+let height = document.querySelector("#height")
+
 function getValue() {
-    let base = Number(document.getElementById("formValue")[0].value)
-    let height = Number(document.getElementById("formValue")[1].value)
-    document.getElementById('resultView').innerHTML = Math.sqrt((base * base) + (height * height)).toFixed(2)
+    if (base.value < 1 || height.value < 1) {
+        document.querySelector('#resultView').innerHTML = "Please Enter a Valid Value"
+    } else {
+        document.querySelector('#resultView').innerHTML = Math.sqrt((base.value * base.value) + (height.value * height.value)).toFixed(2);
+    }
 }
